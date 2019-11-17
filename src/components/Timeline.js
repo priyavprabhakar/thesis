eb0274caaec2792a29695155d6f2e76013ab04a5
Priya Prabhakar <priyaprabhakar@Priyas-MacBook-Pro-2.local>
2019-11-17 00:50:32 -0800

    Made timeline formatted and a black background and a logo.

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,18 +1,22 @@
 import React, {useState} from 'react';
 import '../styles/timeline.css';
+import mainLogo from "../images/logo.png"
 
 function Timeline() {
     const dates = ["1885", "1947", "1954", "1966", "1975", "2000", "2004", "2007", "2009", "2014", "2018", "2019"]
-    const [selectedTab, updateSelectedTab] = useState(dates[0])
+    const [selectedTab, updateSelectedTab] = useState(dates[""])
 
   return (
     <div className="timeline">
-        {
-            dates.map(d => <div className ={`year ${selectedTab === d ? "selectedYear" : ""}`}
+      <img src={mainLogo} className ="mainLogo"/>
+      <div className = "timelineDates"> {
+        dates.map(d => <div className ={`year ${selectedTab === d ? "selectedYear" : ""}`}
             onClick={() => updateSelectedTab(d)} 
             key={d}>{d}</div>)
-        }
+      }
+      </div> 
     </div>
+    
   );
 }
 
