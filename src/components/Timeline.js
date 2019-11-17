652e5f6991ff5aaa15836a78dbd0e1758401c4a2
Priya Prabhakar <priyaprabhakar@Priyas-MacBook-Pro-2.local>
2019-11-16 23:35:51 -0800

    Got timeline to turn red when I choose the date.

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
+import '../styles/timeline.css';
 
 function Timeline() {
+    const dates = ["1885", "1947", "1954", "1966", "1975", "2000", "2004", "2007", "2009", "2014", "2018", "2019"]
+    const [selectedTab, updateSelectedTab] = useState(dates[0])
+
   return (
-    <div className="App">
-        This is my timeline.
+    <div className="timeline">
+        {
+            dates.map(d => <div className ={`year ${selectedTab === d ? "selectedYear" : ""}`}
+            onClick={() => updateSelectedTab(d)} 
+            key={d}>{d}</div>)
+        }
     </div>
   );
 }
