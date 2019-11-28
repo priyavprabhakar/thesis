1d33674da247cbe3671ed2550644afecbf5a2a96
Priya Prabhakar <priyaprabhakar@Priyas-MacBook-Pro-2.local>
2019-11-28 13:00:47 -0600

    Color of dates changed when clicked.

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,7 +1,15 @@
-import React from "react"
+import React, {useState} from "react"
+import {data} from '../utilities/database'
+import '../styles/Timeline.css';
+
 
 function Timeline( ) {
-    return <header>Hello this is the timeline</header>
+    const dates = Object.keys(data)
+    const [selected, updateSelected] = useState(dates[0])
+    console.log(selected)
+    return <div>
+        {dates.map(d => <div onClick={()=> updateSelected(d)} className={`${selected === d ? "selectedDate": ""} date`} key={d}> {d} </div>)}
+    </div>
 }
 
 export default Timeline
---
+++ src/utilities/database.js
@@ -0,0 +1,19 @@
+// keys -- date string
+// value -- object, "text": 
+
+
+export const data = {
+    "1885":{
+        "title": "Indian Telegraph Law",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    "1947":{
+        "title": "The Partition",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    "1954":{
+        "title": "Fundamental Rights Act",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    }
+
+}
