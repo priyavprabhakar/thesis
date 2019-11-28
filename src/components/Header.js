1802e2c77f68ecb1fb05ad6605d2ccef7acd1f75
Priya Prabhakar <priyaprabhakar@Priyas-MacBook-Pro-2.local>
2019-11-28 11:40:44 -0600

    Added the header component and the Timeline as a child.

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Timeline from './Timeline';
+
+function Header( ) {
+    return <header>Hello this is the header
+        <Timeline />
+    </header>
+}
+
+export default Header
---
+++ src/components/Timeline.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Timeline( ) {
+    return <header>Hello this is the timeline</header>
+}
+
+export default Timeline
