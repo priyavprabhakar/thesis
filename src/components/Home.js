17d2cc268295539c46871d2a1c64be9a1f9d70f0
Priya Prabhakar <priyaprabhakar@Priyas-MacBook-Pro-2.local>
2019-11-30 15:27:34 -0600

    Tried to add images to the front page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,13 @@ import {
     BrowserRouter as Router,
     Link,
 } from "react-router-dom";
+import eye_front from '../images/eye_front.png'
+import { homedir } from "os";
 
 function Home() {
     return <div className = "homePageContainer">
+        <img className = "bottom_left" src={bottom_left_home.png} />}
+        <Link className = "eyeFront" to = "/timeline"> {<img className = "eye_front" src={eye_front.png}/>} </Link>
         <div className = "introDataTitle"> {intro_data.title} </div>
         <div className = "introDate"> {intro_data.subtitle} </div>
         <Link to = "/timeline"> {intro_data.button} </Link>
