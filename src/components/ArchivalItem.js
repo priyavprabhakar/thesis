3d75b60c65aecec9e2902044279a400be2bfc533
Divya Prabhakar <divya.prabhakar@stitchfix.com>
2019-12-11 00:34:42 -0800

    Added modal to expand when title is clicked

--- src/components/ArchivalItem.js
+++ src/components/ArchivalItem.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react"
 import { IMAGE, VIDEO } from "../utilities/database"
 import ReactPlayer from "react-player"
+import { Modal, Icon } from "semantic-ui-react"
 
 
 function ArchivalItem(props) {
@@ -8,20 +9,38 @@ function ArchivalItem(props) {
     const {type, source, text, title, date} = props.sourceImage
     
     return <div className="archivalItemContainer" onClick={()=> {
-        if (type === IMAGE) {
-            updateImageShowing(!imageShowing)
+        if (type === IMAGE && imageShowing) {
+            updateImageShowing(false)
         }
-    }} > 
+    }} >
         {type === IMAGE && <div className="archivalItem">
             <img className = "archivalImage" src={source} alt="archival_source"/>
             {!imageShowing && 
+                <>
+                 <Icon name="close" className="archivalItemClose" onClick={() => updateImageShowing(true)}/>
                 <div className="archivalText">
-                    <div className = "titleDateContainer"> 
-                        <div className = "archivalTitle">{title}</div>
-                        <div className = "archivalDate">{date} </div>
-                    </div>
+                    <Modal trigger={
+                        <div className = "titleDateContainer"> 
+                            <div className = "archivalTitle">{title}</div>
+                            <div className = "archivalDate">{date} </div>
+                        </div>
+                    }>
+                        <Modal.Content>
+                            <div className="archivalItemModalContainer">
+                                <img className="archivalModalImage" src={source} alt="archival_modal_img"/>
+                                <div className="archivalModalText">
+                                    <div> 
+                                        <div className="archivalTitle">{title}</div>
+                                        <div className="archivalDate">{date} </div>
+                                    </div>
+                                    <div style={{margin: "16px 0"}}>{text}</div>
+                                </div>
+                            </div>
+                        </Modal.Content>
+                    </Modal>
                     <div>{text}</div>
                 </div>
+                </>
             }
             </div>
         }
